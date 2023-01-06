149febf5e56dfacb7589116e620b2376ced6a8a9
zjq111 <123456@qq.com>
2023-01-06 19:41:28 +0800

    项目初始化

--- doc/sql/create_table.sql
+++ doc/sql/create_table.sql
@@ -22,35 +22,6 @@ create table user
 create index user__name
     on user (username);
 
--- 专利信息表
--- auto-generated definition
-create table patent_information
-(
-    id                    bigint                              not null comment 'id'
-        primary key,
-    applicant             varchar(255)                        null comment '申请人  【某个组织】',
-    patentNumber          varchar(50)                         null comment '专利号',
-    patentName            varchar(255)                        null comment '专利名称',
-    customerId            bigint                              null comment '客户Id  【发明人】',
-    partner               varchar(255)                        null comment '合作方',
-    patentApplicationTime timestamp                           null comment '专利申请日',
-    applicationFeeTime    timestamp                           null comment '申请费缴费时间',
-    authorizationTime     timestamp                           null comment '授权日',
-    billingFeeTime        timestamp                           null comment '办登费缴费时间',
-    nextFeeTime           timestamp                           null comment '下次缴纳年费时间',
-    vintages              int                                 null comment '年费年份',
-    isBill                tinyint                             null comment '是否开票',
-    annualFee             int                                 null comment '年费',
-    createTime            timestamp default CURRENT_TIMESTAMP not null comment '创建时间',
-    updateTime            timestamp default CURRENT_TIMESTAMP null comment '修改时间',
-    isDelete              tinyint   default 0                 null comment '逻辑删除',
-    constraint patent_information_patentNumber_uindex
-        unique (patentNumber)
-)
-    comment '专利信息表';
-
-create index patent_information__name
-    on patent_information (patentName);
 
 
 
